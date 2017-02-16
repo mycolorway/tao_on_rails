1a61dd38e3bf77ef0144aea96a275a858b565c61
farthinker <farthinker@gmail.com>
2017-02-16 17:16:58 +0800

    ADD: tao_page helper

--- lib/tao_on_rails/action_view/helpers.rb
+++ lib/tao_on_rails/action_view/helpers.rb
@@ -3,21 +3,32 @@ module TaoOnRails
     module Helpers
 
       include ::ActionView::Helpers
+      include ::ActionView::Context
 
       def page_id
         controller_names = controller_path.split('/')
         [controller_names, action_name].compact.flatten.join('-')
       end
 
-      def icon_tag(name, options = {})
-        if options[:class].present?
-          options[:class] += " icon icon-#{name}"
+      def icon_tag(name, attributes = {})
+        if attributes[:class].present?
+          attributes[:class] += " icon icon-#{name}"
         else
-          options[:class] = "icon icon-#{name}"
+          attributes[:class] = "icon icon-#{name}"
         end
 
         use_tag = %Q(<use xlink:href="#icon-#{name}"/>).html_safe
-        content_tag(:svg, use_tag, options).html_safe
+        content_tag(:svg, use_tag, attributes).html_safe
+      end
+
+      def tao_page(tag = "#{page_id}-page", attributes, &block)
+        if attributes[:class].present?
+          attributes[:class] += " tao-page"
+        else
+          attributes[:class] = "tao-page"
+        end
+
+        content_tag(tag, capture(&block), attributes)
       end
 
     end
--- test/action_view/helpers_test.rb
+++ test/action_view/helpers_test.rb
@@ -5,7 +5,14 @@ class TaoOnRails::ActionView::HelpersTest < ActiveSupport::TestCase
 
   test 'icon_tag helper' do
     tag = icon_tag :edit, :class => 'test-icon', :'test-attr' => 'test'
-    assert_equal tag, %Q(<svg class=\"test-icon icon icon-edit\" test-attr=\"test\"><use xlink:href=\"#icon-edit\"/></svg>)
+    assert_equal tag, '<svg class="test-icon icon icon-edit" test-attr="test"><use xlink:href="#icon-edit"/></svg>'
+  end
+
+  test 'tao_page helper' do
+    html = tao_page 'test-page', class: 'test-page', layout: 'default', json: {x: 1}.to_json.html_safe do
+      '<header>Page Title</header>'.html_safe
+    end
+    assert_equal html, '<test-page class="test-page tao-page" layout="default" json="{&quot;x&quot;:1}"><header>Page Title</header></test-page>'
   end
 
 end
