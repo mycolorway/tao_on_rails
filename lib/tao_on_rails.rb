298ccf77f425432db48690bf44bb3d706f65a3a4
Terry Tai <poshboytl@gmail.com>
2017-03-16 15:09:42 +0800

    Add the backend for Tao Components. 1. The Base class for Component. 2. Dynamic helper method to use the components. 3. The render strategy in component itself.
    Move the init logic to gem.
    
    pass the view in.
    
    Some tiny refactor.
    
    Use the view passed in from helper.

--- lib/tao_on_rails.rb
+++ lib/tao_on_rails.rb
@@ -1,5 +1,6 @@
 require 'tao_on_rails/version'
 require 'tao_on_rails/engine'
+require 'tao_on_rails/component'
 
 module TaoOnRails
 end
--- lib/tao_on_rails/engine.rb
+++ lib/tao_on_rails/engine.rb
@@ -2,12 +2,16 @@ require 'turbolinks'
 require 'jquery-rails'
 require 'lodash-rails'
 require 'tao_on_rails/action_view/helpers'
+require 'tao_on_rails/action_view/dynamic_helpers'
 
 module TaoOnRails
   class Engine < Rails::Engine
 
     initializer "tao_on_rails.view_helpers" do |app|
-      ActiveSupport.on_load :action_view do
+      ::ActiveSupport.on_load :action_view do
+        TaoOnRails::ActionView::DynamicHelpers.init_components
+
+        include TaoOnRails::ActionView::DynamicHelpers
         include TaoOnRails::ActionView::Helpers
       end
     end
