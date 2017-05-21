357256f6286e0c109c29a7633ba8d01dc7dc48ce
farthinker <farthinker@gmail.com>
2017-05-21 22:47:57 +0800

    CHG: fix generator test

--- test/generators/assets_generator_test.rb
+++ test/generators/assets_generator_test.rb
@@ -7,33 +7,33 @@ class Tao::Generators::AssetsGeneratorTest < Rails::Generators::TestCase
   tests Tao::Generators::AssetsGenerator
 
   test 'generate assets files' do
-    assert_no_file 'app/assets/javascripts/employees/index.coffee'
-    assert_no_file 'app/assets/stylesheets/employees/index.scss'
+    assert_no_file 'app/assets/javascripts/employees/index_page.coffee'
+    assert_no_file 'app/assets/stylesheets/employees/index_page.scss'
     run_generator %w(employee index)
-    assert_file 'app/assets/javascripts/employees/index.coffee' do |content|
+    assert_file 'app/assets/javascripts/employees/index_page.coffee' do |content|
       assert_match(/class EmployeeIndexPage extends TaoPage/, content)
     end
-    assert_file 'app/assets/stylesheets/employees/index.scss' do |content|
+    assert_file 'app/assets/stylesheets/employees/index_page.scss' do |content|
       assert_match(/employee-index-page \{/, content)
     end
   end
 
   test 'generate assets files with variants' do
-    assert_no_file 'app/assets/javascripts/employees/default/index.coffee'
-    assert_no_file 'app/assets/stylesheets/employees/default/index.scss'
-    assert_no_file 'app/assets/javascripts/employees/phone/index.coffee'
-    assert_no_file 'app/assets/stylesheets/employees/phone/index.scss'
+    assert_no_file 'app/assets/javascripts/employees/default/index_page.coffee'
+    assert_no_file 'app/assets/stylesheets/employees/default/index_page.scss'
+    assert_no_file 'app/assets/javascripts/employees/phone/index_page.coffee'
+    assert_no_file 'app/assets/stylesheets/employees/phone/index_page.scss'
     run_generator %w(employee index --variants=default phone)
-    assert_file 'app/assets/javascripts/default/employees/index.coffee' do |content|
+    assert_file 'app/assets/javascripts/default/employees/index_page.coffee' do |content|
       assert_match(/class EmployeeIndexPage extends TaoPage/, content)
     end
-    assert_file 'app/assets/stylesheets/default/employees/index.scss' do |content|
+    assert_file 'app/assets/stylesheets/default/employees/index_page.scss' do |content|
       assert_match(/employee-index-page \{/, content)
     end
-    assert_file 'app/assets/javascripts/phone/employees/index.coffee' do |content|
+    assert_file 'app/assets/javascripts/phone/employees/index_page.coffee' do |content|
       assert_match(/class EmployeeIndexPage extends TaoPage/, content)
     end
-    assert_file 'app/assets/stylesheets/phone/employees/index.scss' do |content|
+    assert_file 'app/assets/stylesheets/phone/employees/index_page.scss' do |content|
       assert_match(/employee-index-page \{/, content)
     end
   end
