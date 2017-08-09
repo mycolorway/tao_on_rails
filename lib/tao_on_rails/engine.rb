ea9cbd471c2092a1d50e86858165ecc8f28a27d4
farthinker <farthinker@gmail.com>
2017-08-09 16:49:16 +0800

    CHG: auto load path

--- lib/tao_on_rails/engine.rb
+++ lib/tao_on_rails/engine.rb
@@ -5,7 +5,7 @@ require 'lodash-rails'
 module TaoOnRails
   class Engine < Rails::Engine
 
-    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    config.eager_load_paths += Dir["#{config.root}/lib"]
 
     initializer "tao_on_rails" do
       ::ActiveSupport.on_load :action_view do
