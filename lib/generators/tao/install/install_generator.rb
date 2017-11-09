e8c9e0bc5cc8dcba8e8dad4ca82aa7f44480675e
farthinker <farthinker@gmail.com>
2017-11-09 11:43:33 +0800

    CHG: fix eager load issue

--- lib/generators/tao/install/install_generator.rb
+++ lib/generators/tao/install/install_generator.rb
@@ -1,35 +1,35 @@
+require 'rails/generators'
+
 module Tao
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-      def install_tao
-        assert_rails_version
+    def install_tao
+      assert_rails_version
 
-        gem 'tao_on_rails'
+      gem 'tao_on_rails'
 
-        # remove_file 'app/assets/javascripts/cable.js'
-        # remove_file 'app/assets/javascripts/application.js'
-        # remove_file 'app/assets/stylesheets/application.css'
-        #
-        # template 'app/assets/javascripts/application.coffee'
-        # template 'app/assets/stylesheets/application.scss'
-        # template 'app/assets/stylesheets/_globals.scss'
-        #
-        # template 'app/views/layouts/application.html.erb', force: true
+      # remove_file 'app/assets/javascripts/cable.js'
+      # remove_file 'app/assets/javascripts/application.js'
+      # remove_file 'app/assets/stylesheets/application.css'
+      #
+      # template 'app/assets/javascripts/application.coffee'
+      # template 'app/assets/stylesheets/application.scss'
+      # template 'app/assets/stylesheets/_globals.scss'
+      #
+      # template 'app/views/layouts/application.html.erb', force: true
 
-        template 'app/components/application_component.rb'
-      end
-
-      private
-
-      def assert_rails_version
-        requirement = Gem::Requirement.new('>= 5.0.0')
-        rails_version = Gem::Version.new(Rails::VERSION::STRING)
-        return if requirement.satisfied_by?(rails_version)
-        fail Rails::Generators::Error, 'Rails >= 5.0.0 is required'
-      end
-
+      template 'app/components/application_component.rb'
     end
+
+    private
+
+    def assert_rails_version
+      requirement = Gem::Requirement.new('>= 5.0.0')
+      rails_version = Gem::Version.new(Rails::VERSION::STRING)
+      return if requirement.satisfied_by?(rails_version)
+      fail Rails::Generators::Error, 'Rails >= 5.0.0 is required'
+    end
+
   end
 end
