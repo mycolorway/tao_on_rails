0bfc27e8a9c0374fd08348f9008347f2aa824cbe
farthinker <farthinker@gmail.com>
2017-11-18 17:15:40 +0800

    CHG: fix test issue

--- lib/generators/tao/component/component_generator.rb
+++ lib/generators/tao/component/component_generator.rb
@@ -1,19 +1,20 @@
 require 'rails/generators'
 
 module Tao
+  module Generators
 
-  class ComponentGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path('../templates', __FILE__)
+    class ComponentGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
 
-    check_class_collision suffix: "Component"
+      check_class_collision suffix: "Component"
 
-    attr_reader :component_name
+      attr_reader :component_name
 
-    def create_component_file
-      @component_name = class_name.underscore.split('/').map(&:singularize).join('_')
-      template "component.rb.erb", File.join('app/components', class_path, "#{file_name}_component.rb")
+      def create_component_file
+        @component_name = class_name.underscore.split('/').map(&:singularize).join('_')
+        template "component.rb.erb", File.join('app/components', class_path, "#{file_name}_component.rb")
+      end
+
     end
-
   end
-
 end
